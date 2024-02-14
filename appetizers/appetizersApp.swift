c2b2a20f5df7f6b68a019b351decaf06097538a5
soten <sotekrg@gmail.com>
2024-02-14 15:50:18 +0600

    Initial Commit

---
+++ appetizers/appetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  appetizersApp.swift
+//  appetizers
+//
+//  Created by Станислав Москальцов  on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct appetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
